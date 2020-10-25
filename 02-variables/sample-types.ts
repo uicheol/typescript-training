59c13ef2ceb01aed5f2219a97d8983db88ce2da3
uicheol.choi <uicheol@sixshop.com>
2020-10-25 21:44:52 +0900

    <feat> types example / add gitignore file

---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "uicheol";
+let lastName: string = 'Choi';
+let any: any = "string";
+
+console.log(found);
+console.log("grade  is " + grade);
+console.log("hi " + firstName + " " + lastName);
+console.log(`Hi ${firstName} ${lastName}`);
